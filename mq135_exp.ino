c3f55f92ff654686b4ba6dcee8580f042f04455f
Kajidataonline <57325497+ismailsakdo@users.noreply.github.com>
2023-02-15 12:38:23 +0800

    Update mq135_exp.ino

--- mq135_exp.ino
+++ mq135_exp.ino
@@ -1,28 +1,22 @@
 /*
-Adapted from: https://microcontrollerslab.com/interfacing-mq-135-gas-sensor-arduino/
 */
 
-int sensorValue;
-int digitalValue;
+// These constants won't change. They're used to give names to the pins used:
+const int analogInPin = A0;  // Analog input pin that the potentiometer is attached to
+int sensorValue = 0;        // value read from the pot
 
-void setup()
-{
-  Serial.begin(9600); // sets the serial port to 9600
-  pinMode(13, OUTPUT);
-  pinMode(2, INPUT);
+void setup() {
+  // initialize serial communications at 9600 bps:
+  Serial.begin(9600);
 }
 
-void loop()
-{
-  sensorValue = analogRead(0); // read analog input pin 0
-  digitalValue = digitalRead(2);
-  if (sensorValue > 400)
-  {
-    digitalWrite(13, HIGH);
-  }
-  else
-    digitalWrite(13, LOW);
-  Serial.println(sensorValue, DEC); // prints the value read
-  Serial.println(digitalValue, DEC);
-  delay(1000); // wait 100ms for next reading
+void loop() {
+  // read the analog in value:
+  sensorValue = analogRead(analogInPin);
+  Serial.print("sensor = ");
+  Serial.println(sensorValue);
+
+  // wait 2 milliseconds before the next loop for the analog-to-digital
+  // converter to settle after the last reading:
+  delay(1000);
 }
