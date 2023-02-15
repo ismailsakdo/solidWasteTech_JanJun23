92a09718128b19eefee1beab8b6d55b07aea6735
Kajidataonline <57325497+ismailsakdo@users.noreply.github.com>
2023-02-15 12:32:38 +0800

    Create mq135_exp.ino

---
+++ mq135_exp.ino
@@ -0,0 +1,28 @@
+/*
+Adapted from: https://microcontrollerslab.com/interfacing-mq-135-gas-sensor-arduino/
+*/
+
+int sensorValue;
+int digitalValue;
+
+void setup()
+{
+  Serial.begin(9600); // sets the serial port to 9600
+  pinMode(13, OUTPUT);
+  pinMode(2, INPUT);
+}
+
+void loop()
+{
+  sensorValue = analogRead(0); // read analog input pin 0
+  digitalValue = digitalRead(2);
+  if (sensorValue > 400)
+  {
+    digitalWrite(13, HIGH);
+  }
+  else
+    digitalWrite(13, LOW);
+  Serial.println(sensorValue, DEC); // prints the value read
+  Serial.println(digitalValue, DEC);
+  delay(1000); // wait 100ms for next reading
+}
